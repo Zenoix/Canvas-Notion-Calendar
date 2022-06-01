35c32d3259ce23604e444ff531d222cdacdd7b30
Zenoix <41812358+Zenoix@users.noreply.github.com>
2022-06-01 15:47:20 +1200

    Moved notion api code into a separate file

---
+++ src/notion.py
@@ -0,0 +1,75 @@
+import os
+import requests
+import json
+from datetime import date
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+url = "https://api.notion.com/v1/pages"
+database_id = os.getenv("NOTION_DATABASE_ID")
+
+# for testing
+todays_date = date.today().strftime("%Y-%m-%d")
+
+headers = {
+    "Authorization": os.getenv("NOTION_KEY"),
+    "Accept": "application/json",
+    "Notion-Version": "2022-02-22",
+    "Content-Type": "application/json"
+}
+
+payload = {
+    "parent": {"type": "database_id",
+               "database_id": database_id},
+    "properties": {
+        "title": {
+            "title": [
+                {
+                    "type": "text",
+                    "text": {
+                        "content": "API Test"
+                    }
+                }
+            ]
+        },
+        "date": {
+            "date": {
+                "start": todays_date
+            }
+        },
+        "Completed": {
+            "type": "checkbox",
+            "checkbox": False
+        },
+        "Website": {
+            "type": "url",
+            "url": None
+        },
+        "Files": {
+            "type": "rich_text",
+            "rich_text": []
+        },
+        "module": {
+            "type": "select",
+            "select": {
+                "name": "test"
+            }
+        },
+        "notes": {
+              "type": "rich_text",
+              "rich_text": []
+            },
+        "Type": {
+              "type": "multi_select",
+              "multi_select": [{
+                  "name": "api_tests"
+              }]
+            },
+    }
+}
+
+response = requests.post(url, json=payload, headers=headers)
+
+print(json.dumps(response.json(), indent=2))
