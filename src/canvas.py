1ea9e81418b01317efffebc242c48fd55c14592e
Zenoix <41812358+Zenoix@users.noreply.github.com>
2022-07-25 13:45:08 +1200

    Managed to login to canvas through headless browser

--- src/canvas.py
+++ src/canvas.py
@@ -1,20 +1,32 @@
-from time import sleep
+import time
 from getpass import getpass
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 
 
 def access_canvas(url):
+    user = input("Username or Email: ")
     password = getpass()
-    sleep(2)
+    time.sleep(2)
 
     options = Options()
-    # options.headless = True
+    options.headless = True
 
     browser = webdriver.Chrome(options=options)
     browser.get(url)
 
-    print(browser.title)
+    user_input = browser.find_element(By.ID, "username")
+    user_input.send_keys(user)
+
+    password_input = browser.find_element(By.ID, "password")
+    password_input.send_keys(password)
+
+    browser.find_element(By.ID, "_eventId_proceed").click()
+
+    time.sleep(2)
+
+    print("Successfully logged in!" if browser.title == "Dashboard" else "Failed logging in")
 
     browser.close()
