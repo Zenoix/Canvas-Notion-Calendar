c1ddd8ff46f75a78bc6eb973c7e5ed84aeee78d7
Zenoix <41812358+Zenoix@users.noreply.github.com>
2022-07-26 10:57:29 +1200

    Renamed browser variable to driver

--- src/canvas.py
+++ src/canvas.py
@@ -17,42 +17,42 @@ def canvas_login(canvas_url: str, username: str, password: str) -> webdriver.Chr
     options = Options()
     options.headless = True
 
-    browser = webdriver.Chrome(options=options)
-    browser.get(canvas_url)
+    driver = webdriver.Chrome(options=options)
+    driver.get(canvas_url)
 
-    user_input = browser.find_element(By.ID, "username")
+    user_input = driver.find_element(By.ID, "username")
     user_input.send_keys(username)
 
-    password_input = browser.find_element(By.ID, "password")
+    password_input = driver.find_element(By.ID, "password")
     password_input.send_keys(password)
 
-    browser.find_element(By.ID, "_eventId_proceed").click()
+    driver.find_element(By.ID, "_eventId_proceed").click()
 
     time.sleep(2)
 
-    if browser.title == "Dashboard":
+    if driver.title == "Dashboard":
         print("Successfully logged in!")
-        return browser
+        return driver
     else:
         print("Failed logging in.")
-        browser.close()
+        driver.close()
 
 
 def create_requests_session() -> requests.sessions.Session:
     return requests.Session()
 
 
-def transfer_cookies(session: requests.sessions.Session, browser: webdriver.Chrome) -> requests.sessions.Session:
+def transfer_cookies(session: requests.sessions.Session, driver: webdriver.Chrome) -> requests.sessions.Session:
     # Set correct user agent
-    selenium_user_agent = browser.execute_script("return navigator.userAgent;")
+    selenium_user_agent = driver.execute_script("return navigator.userAgent;")
     session.headers.update({"user-agent": selenium_user_agent})
 
-    for cookie in browser.get_cookies():
+    for cookie in driver.get_cookies():
         session.cookies.set(cookie['name'], cookie['value'], domain=cookie['domain'])
 
     return session
 
 
-def close_all_connections(session: requests.sessions.Session, browser: webdriver.Chrome) -> None:
+def close_all_connections(session: requests.sessions.Session, driver: webdriver.Chrome) -> None:
     session.close()
-    browser.close()
+    driver.close()
