3db971f2941b0db541cdb77fa1e50410ea5ebdeb
Zenoix <41812358+Zenoix@users.noreply.github.com>
2022-07-26 11:07:41 +1200

    Created an api interface class

--- src/canvas.py
+++ src/canvas.py
@@ -7,52 +7,52 @@ from selenium.webdriver.common.by import By
 import requests
 
 
-def get_canvas_login() -> tuple[str, str]:
-    user = input("Username or Email: ")
-    password = getpass()
-    return user, password
+class CanvasAPIInterface:
+    def __init__(self, canvas_url: str = None):
+        self.__canvas_url = canvas_url if canvas_url else input()
+        self.__username = None
+        self.__password = None
+        self.__driver = None
+        self.__session = None
 
+    def get_canvas_login(self) -> None:
+        self.__username = input("Username or Email: ")
+        self.__password = getpass()
 
-def canvas_login(canvas_url: str, username: str, password: str) -> webdriver.Chrome | None:
-    options = Options()
-    options.headless = True
+    def canvas_login(self) -> None:
+        options = Options()
+        options.headless = True
 
-    driver = webdriver.Chrome(options=options)
-    driver.get(canvas_url)
+        self.__driver = webdriver.Chrome(options=options)
+        self.__driver.get(self.__canvas_url)
 
-    user_input = driver.find_element(By.ID, "username")
-    user_input.send_keys(username)
+        user_input = self.__driver.find_element(By.ID, "username")
+        user_input.send_keys(self.__username)
 
-    password_input = driver.find_element(By.ID, "password")
-    password_input.send_keys(password)
+        password_input = self.__driver.find_element(By.ID, "password")
+        password_input.send_keys(self.__password)
 
-    driver.find_element(By.ID, "_eventId_proceed").click()
+        self.__driver.find_element(By.ID, "_eventId_proceed").click()
 
-    time.sleep(2)
+        time.sleep(2)
 
-    if driver.title == "Dashboard":
-        print("Successfully logged in!")
-        return driver
-    else:
-        print("Failed logging in.")
-        driver.close()
+        if self.__driver.title == "Dashboard":
+            print("Successfully logged in!")
+        else:
+            print("Failed logging in.")
+            self.__driver.close()
 
+    def create_requests_session(self) -> None:
+        self.__session = requests.Session()
 
-def create_requests_session() -> requests.sessions.Session:
-    return requests.Session()
+    def transfer_cookies(self):
+        # Set correct user agent
+        selenium_user_agent = self.__driver.execute_script("return navigator.userAgent;")
+        self.__session.headers.update({"user-agent": selenium_user_agent})
 
+        for cookie in self.__driver.get_cookies():
+            self.__session.cookies.set(cookie['name'], cookie['value'], domain=cookie['domain'])
 
-def transfer_cookies(session: requests.sessions.Session, driver: webdriver.Chrome) -> requests.sessions.Session:
-    # Set correct user agent
-    selenium_user_agent = driver.execute_script("return navigator.userAgent;")
-    session.headers.update({"user-agent": selenium_user_agent})
-
-    for cookie in driver.get_cookies():
-        session.cookies.set(cookie['name'], cookie['value'], domain=cookie['domain'])
-
-    return session
-
-
-def close_all_connections(session: requests.sessions.Session, driver: webdriver.Chrome) -> None:
-    session.close()
-    driver.close()
+    def close_all_connections(self) -> None:
+        self.__session.close()
+        self.__driver.close()
