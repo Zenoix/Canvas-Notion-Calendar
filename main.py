f1e2e534c23c392732d7fba325023af3fde10e98
Zenoix <41812358+Zenoix@users.noreply.github.com>
2022-07-28 23:53:08 +1200

    Started laying out the main file

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 from src.canvas import CanvasAPIInterface
-# from src.notion import ...
+from src.notion import NotionAPIInterface
+
+
+def main() -> None:
+    c = CanvasAPIInterface()
+    c.run()
+    assignments = c.assignments
+
+    n = NotionAPIInterface()
+    n.assignments = assignments
+    n.run()
+
 
 if __name__ == "__main__":
-    pass
+    main()
